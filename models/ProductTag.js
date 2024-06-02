8c9449c336218d0e17562c86e6cce15662465093
harrismhurley <harrismhurley@icloud.com>
2024-06-02 05:23:00 -0500

    added productTag.js rows

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,42 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
+// Define the ProductTag model by extending the Sequelize Model class
 class ProductTag extends Model {}
 
+// Initialize the ProductTag model with column definitions
 ProductTag.init(
   {
-    // define columns
+    // Define the id column
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true,
+    },
+    // Define the product_id column
+    product_id: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: 'Product',
+        key: 'id',
+      },
+    },
+    // Define the tag_id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag', 
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
+    freezeTableName: true, 
+    underscored: true, 
+    modelName: 'product_tag', 
   }
 );
 
