258765e098d8bf480ab235da62bdad33cd47e814
harrismhurley <harrismhurley@icloud.com>
2024-06-02 05:27:42 -0500

    added comments to all models, and the tag model

--- models/Product.js
+++ models/Product.js
@@ -7,17 +7,19 @@ class Product extends Model {}
 // Set up fields and rules for Product model
 Product.init(
   {
-    // Define columns
+    // Define the id column
     id: {
       type: DataTypes.INTEGER, 
       allowNull: false, 
       primaryKey: true, 
       autoIncrement: true, 
     },
+    // Define the product_name column
     product_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Define the price column
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false, 
@@ -25,6 +27,7 @@ Product.init(
         isDecimal: true,
       },
     },
+    // Define the stock column
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false, 
@@ -33,6 +36,7 @@ Product.init(
         isNumeric: true, 
       },
     },
+    // Define the category_id column
     category_id: {
       type: DataTypes.INTEGER,
       references: {
--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,29 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
+// Initialize the Tag model by extending from Sequelize's Model class
 Tag.init(
   {
-    // define columns
+    // Define the id column
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true, 
+      autoIncrement: true, 
+    },
+    // Define the tag_name column
+    tag_name: {
+      type: DataTypes.STRING, 
+    },
   },
   {
     sequelize,
     timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
+    freezeTableName: true, 
+    underscored: true, 
+    modelName: 'tag', 
   }
 );
 
